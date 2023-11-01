1a318bb7f25f58f52738f2abd563d5432125e3b5
Anton Maminov <anton.linux@gmail.com>
2023-11-01 14:24:06 +0200

    Remove the alias for ERB::Util.t to prevent conflicts with the I18n.t method

--- lib/redcloth/erb_extension.rb
+++ lib/redcloth/erb_extension.rb
@@ -3,14 +3,14 @@ class ERB
 
     #
     # A utility method for transforming Textile in _s_ to HTML.
-    # 
+    #
     # 	require "erb"
     # 	include ERB::Util
-    # 	
+    #
     # 	puts textilize("Isn't ERB *great*?")
-    # 
+    #
     # _Generates_
-    # 
+    #
     # 	<p>Isn&#8217;t <span class="caps">ERB</span> <strong>great</strong>?</p>
     #
     def textilize( s )
@@ -19,8 +19,6 @@ class ERB
       end
     end
 
-    alias t textilize
-    module_function :t
     module_function :textilize
 
   end
